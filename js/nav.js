681c3b8a3b0a67fba9c12b0099fd4a1581080960
zakariyamahamud10-gmail <zakariyamahamud10@gmail.com>
2026-02-05 13:14:34 +0100

    nälla funka

--- js/nav.js
+++ js/nav.js
@@ -8,32 +8,41 @@ export function createNav(currentPage) {
     if (!navList) return;
 
     navList.innerHTML = "";
-  
+
     for (let i = 0; i < assignments.length; i++) {
         const a = assignments[i];
 
         let activeClass = "";
 
-        if(a.id === currentPage) {
+        if (a.id === currentPage) {
             activeClass = "active";
         }
 
         let link = a.link;
-
-        if(currentPage !== "home") {
-            if(a.id === "home") {
-                link = "../index.html"
-            }
+   // Om vi är på en undersida (inte home)
+    if (currentPage !== "home") {
+      if (a.id === "home") {
+        // till startsidan: gå upp en nivå
+        link = "../index.html";
+      } else if (a.id === currentPage) {
+        // till samma sida: håll dig i mappen
+        link = "index.html";
+      } else {
+        // till andra uppgifter: gå upp och sedan ner
+        link = "../" + a.link;
+      }
         }
 
-          // Skapar en länk i navigationen, här istället för att skriva ut det i html
-    navList.innerHTML += `
+
+
+        // Skapar en länk i navigationen, här istället för att skriva ut det i html
+        navList.innerHTML += `
       <li>
         <a class="${activeClass}" href="${link}">
           ${a.title}
         </a>
       </li>
     `;
-  }
+    }
 }
-    
+
